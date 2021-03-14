ea951e19e5f109b37d31967ee78d3402eeaaca37
AaronMikelKey <buckshot307@gmail.com>
2021-03-14 18:04:09 -0230

    Added content-2 on homepage
    Used fontawesome to list experience

--- src/dir/Home.js
+++ src/dir/Home.js
@@ -21,7 +21,67 @@ const Home = () => {
               <img src='' alt='An image of myself' />
             </FadeIn>
         <div className='content-2'>
-          Content 2 here
+          <h3 className='title'>Experience with: </h3>
+          <span className='item'>
+            <i className='fab fa-aws' />
+            Amazon Web Services
+          </span>
+          <span className='item'>
+            <i className='fab fa-apple' />
+            Apple
+          </span>
+          <span className='item'>
+            <i className='fab fa-cpanel' />
+            cPanel
+          </span>
+          <span className='item'>
+            <i className='fab fa-css3-alt' />
+            CSS3
+          </span>
+          <span className='item'>
+            <i className='fab fa-github' />
+            Github
+          </span>
+          <span className='item'>
+            <i className='fab fa-html5' />
+            HTML5
+          </span>
+          <span className='item'>
+            <i className='fab fa-js-square' />
+            Javascript
+          </span>
+          <span className='item'>
+            <i className='fab fa-less' />
+            Less
+          </span>
+          <span className='item'>
+            <i className='fab fa-linux' />
+            Linux
+          </span>
+          <span className='item'>
+            <i className='fab fa-mdb' />
+            MongoDB
+          </span>
+          <span className='item'>
+            <i className='fab fa-node' />
+            NodeJS
+          </span>
+          <span className='item'>
+            <i className='fab fa-npm' />
+            NPM
+          </span>
+          <span className='item'>
+            <i className='fab fa-react' />
+            React
+          </span>
+          <span className='item'>
+            <i className='fab fa-sass' />
+            Sass
+          </span>
+          <span className='item'>
+            <i className='fab fa-windows' />
+            Windows
+          </span>
         </div>
           <div className='content-3'>
             content 3 here
