6827555286bf44a33d2ca4d95419bf5805d13823
Aaron Key <AaronMikelKey@gmail.com>
2021-04-03 21:57:13 -0400

    Added info and css for contact page

--- src/dir/Contact.js
+++ src/dir/Contact.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../css/contact.sass'
 
 const Contact = () => {
   return (
-    <div>
-      This is the Contact page.
+    <div className='contact'>
+      <div className='content'>
+      <header>
+        <h1>Contact</h1>
+      </header>
+      <div className='contact-grid'>
+        <a href='https://github.com/AaronMikelKey' target='_blank' className='a1'><i className='fab fa-github' aria-hidden="true"></i><span> Github</span></a>
+        <a href="www.linkedin.com/in/AaronKey" target="_blank" className='a2'><i className="fab fa-linkedin" aria-hidden="true"></i><span> LinkedIn</span></a>
+        <a href="mailto:AaronMikelKey@gmail.com" target="_parent" className='a3'><i className="fa fa-envelope" aria-hidden="true"></i><span> Email</span></a>
+        </div>
+      </div>
     </div>
   )
 }
