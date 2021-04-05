c53c2353017d82b5bee1570ff3490af410ade597
Aaron Key <AaronMikelKey@gmail.com>
2021-04-05 14:38:47 -0400

    Added first project and css for project page

--- src/dir/Projects.js
+++ src/dir/Projects.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import jandkscreenshot from '../css/photos/JandkScreenshot.png'
+import '../css/projects.sass'
 
 const Projects = () => {
   return (
-    <div>
-      This is the Projects page.
+    <div className='container-p'>
+      <header className='header-p'>
+        <h1>Projects</h1>
+        <p>Here's a few of the projects I've made.</p>
+      </header>
+      <a href='https://jandkpaintsandpix.com' target='_blank' className='p1'>
+        <h2>J&amp;K Paints and Pixels</h2>
+        <img src={jandkscreenshot}></img>
+        <p className='description'>A simple react website for a local business</p>
+      </a>
     </div>
   )
 }
