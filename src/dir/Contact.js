23e7946548195726f5511ce5bc690e9a0263a6c0
AaronMikelKey <AaronMikelKey@gmail.com>
2021-04-26 14:26:55 -0400

    Fixed css errors on about
    h1 font size was too large causing overflow, reduced from 3em to 2em.  p letter-spacing was too small making it difficult to read, changed to 3px

--- src/dir/Contact.js
+++ src/dir/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
       </header>
       <div className='contact-grid'>
         <a href='https://github.com/AaronMikelKey' target='_blank' className='a1'><i className='fab fa-github' aria-hidden="true"></i><span> Github</span></a>
-        <a href="www.linkedin.com/in/AaronKey" target="_blank" className='a2'><i className="fab fa-linkedin" aria-hidden="true"></i><span> LinkedIn</span></a>
+        <a href="https://linkedin.com/in/AaronKey" target="_blank" className='a2'><i className="fab fa-linkedin" aria-hidden="true"></i><span> LinkedIn</span></a>
         <a href="mailto:AaronMikelKey@gmail.com" target="_parent" className='a3'><i className="fa fa-envelope" aria-hidden="true"></i><span> Email</span></a>
         </div>
       </div>
