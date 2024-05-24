53aa2a596e54d51c85fa644cd4ac6c85b859bab3
Aaron Key <aaronmikelkey@gmail.com>
2024-05-24 01:08:01 -0400

    change resize timeout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import {isIE} from 'react-device-detect'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom"
+import { isIE } from 'react-device-detect'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import NavBurger from './components/NavBurger'
 import About from './dir/About'
@@ -14,29 +10,30 @@ import Home from './dir/Home'
 import DevTest from './dir/DevTest'
 import ScrollTop from './components/ScrollTop'
 
-
-
 const App = () => {
-
-  {/* Determines lightmode or darkmode */}
-  const [themeName, changeTheme] = useState('darkmode')
+	{
+		/* Determines lightmode or darkmode */
+	}
+	const [themeName, changeTheme] = useState('darkmode')
 	const [offset, setOffset] = useState(0)
 	const [scrolled, setScrolled] = useState(false)
-  {/* State setting for fade in elements */}
-  const [loaded, setLoaded] = useState({ 
-    home: {
-      content1: false,
-      content2: false
-    },
-    about: {
-      content1: false,
-      content2: false
-    },
-    projects: {
-      content1: false,
-      content2: false
-    }
-  })
+	{
+		/* State setting for fade in elements */
+	}
+	const [loaded, setLoaded] = useState({
+		home: {
+			content1: false,
+			content2: false,
+		},
+		about: {
+			content1: false,
+			content2: false,
+		},
+		projects: {
+			content1: false,
+			content2: false,
+		},
+	})
 
 	const getWidth = () => {
 		return document.body.clientWidth
@@ -48,7 +45,7 @@ const App = () => {
 			let timeout = null
 			const resizeListener = () => {
 				clearTimeout(timeout)
-				timeout = setTimeout(() => setWidth(getWidth()), 250) // Keeps the resize listener from running too many times if someone is resizing their window.
+				timeout = setTimeout(() => setWidth(getWidth()), 50) // Keeps the resize listener from running too many times if someone is resizing their window.
 			}
 			window.addEventListener('resize', resizeListener)
 
@@ -58,72 +55,72 @@ const App = () => {
 		}, [])
 		return width
 	}
-	
 
-
-  if (isIE) return <div>Internet Explorer is not supported.  Please download Firefox, Chrome, or another web browser.</div>
-  const changeThemeClick = () => {
-    let l = document.querySelectorAll('.lightmode');
-    let d = document.querySelectorAll('.darkmode');
-    if (themeName === 'darkmode') {
-      changeTheme('lightmode')
-    } else {
-      changeTheme('darkmode')
-    }
-    for (let i=0;i<l.length;i++) {
-      l[i].classList.add('darkmode')
-      l[i].classList.remove('lightmode')
-    }
-    for (let k=0;k<d.length;k++) {
-      d[k].classList.add('lightmode')
-      d[k].classList.remove('darkmode')
-    }
-  }
-
-	const Nav = (props) => {
+	if (isIE)
+		//Probably don't need this still but who knows.
 		return (
-			width < 1080 ? <NavBurger {...props} /> : <Navbar {...props}/>
+			<div>
+				Internet Explorer is not supported. Please download Firefox, Chrome, or
+				another web browser.
+			</div>
 		)
+	const changeThemeClick = () => {
+		let l = document.querySelectorAll('.lightmode')
+		let d = document.querySelectorAll('.darkmode')
+		if (themeName === 'darkmode') {
+			changeTheme('lightmode')
+		} else {
+			changeTheme('darkmode')
+		}
+		for (let i = 0; i < l.length; i++) {
+			l[i].classList.add('darkmode')
+			l[i].classList.remove('lightmode')
+		}
+		for (let k = 0; k < d.length; k++) {
+			d[k].classList.add('lightmode')
+			d[k].classList.remove('darkmode')
+		}
 	}
 
+	const Nav = (props) => {
+		return width < 1080 ? <NavBurger {...props} /> : <Navbar {...props} />
+	}
 
 	useCurrentWidth()
-  return (
-    <Router>
-      <ScrollTop />
-      <Nav themeName={themeName} changeTheme={changeThemeClick} />
-      <Switch>
+	return (
+		<Router>
+			<ScrollTop />
+			<Nav themeName={themeName} changeTheme={changeThemeClick} />
+			<Switch>
+				<Route path="/about">
+					<About loaded={loaded} setLoaded={setLoaded} />
+				</Route>
 
-        <Route path="/about">
-          <About loaded={loaded} setLoaded={setLoaded} />
-        </Route>
+				<Route path="/contact">
+					<Contact loaded={loaded} setLoaded={setLoaded} />
+				</Route>
 
-        <Route path="/contact">
-          <Contact loaded={loaded} setLoaded={setLoaded} />
-        </Route>
+				<Route path="/projects">
+					<Projects loaded={loaded} setLoaded={setLoaded} />
+				</Route>
 
-        <Route path="/projects">
-          <Projects loaded={loaded} setLoaded={setLoaded} />
-        </Route>
+				<Route path="/devtest">
+					<DevTest loaded={loaded} setLoaded={setLoaded} />
+				</Route>
 
-        <Route path="/devtest">
-          <DevTest loaded={loaded} setLoaded={setLoaded} />
-        </Route>
-
-        <Route path="/">
-          <Home 
-						loaded={loaded} 
-						setLoaded={setLoaded} 
-						offset={offset} 
+				<Route path="/">
+					<Home
+						loaded={loaded}
+						setLoaded={setLoaded}
+						offset={offset}
 						setOffset={setOffset}
 						scrolled={scrolled}
-						setScrolled={setScrolled} 
-						/>
-        </Route>
-
-      </Switch>
-    </Router>
-  )
+						setScrolled={setScrolled}
+					/>
+				</Route>
+			</Switch>
+		</Router>
+	)
 }
 
 export default App
